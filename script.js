16eec89d042ef953d2bdc7943f91e629439b4c64
Mitchell Reid <mitchreid14@gmail.com>
2020-10-31 16:05:21 -0400

    updates

--- script.js
+++ script.js
@@ -18,22 +18,21 @@ const randomFunc = {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+}
+  function getRandomUpperCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  function getRandomLowerCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  function getRandomSymbol() {
+    var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+    return symbol[Math.floor(Math.random() * symbol.length)];
+  }
 
-  passwordText.value = password;
-}
-function getRandomUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getRandomLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol() {
-  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random() * symbol.length)];
-}
 
 // Add event listener to generate button
 generateEl.addEventListener('click', () => {
